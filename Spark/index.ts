4bdead97aaf5476d7932e59d65ef50bf3d5c0dfe
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-07-20 19:50:59 +0900

    chore: create project by Azure

---
+++ Spark/index.ts
@@ -0,0 +1,12 @@
+import { AzureFunction, Context } from "@azure/functions";
+
+const timerTrigger: AzureFunction = async function(context: Context, myTimer: any): Promise<void> {
+  var timeStamp = new Date().toISOString();
+
+  if (myTimer.IsPastDue) {
+    context.log("Timer function is running late!");
+  }
+  context.log("Timer trigger function ran!", timeStamp);
+};
+
+export default timerTrigger;
